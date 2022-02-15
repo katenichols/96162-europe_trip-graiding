81b27685c3ff17e071733a49dd0b7fae788835ca
e.suslova <e.suslova@skbkontur.ru>
2022-02-16 00:11:21 +0500

    Сделан блок promo. У кнопки сделать переход к табам

--- source/js/script.js
+++ source/js/script.js
@@ -3,18 +3,18 @@ const navList = document.querySelector(".nav__list");
 const navListItems = document.querySelectorAll(".nav__list-item");
 
 navButton.addEventListener("click", () => {
-  if (navButton.classList.contains("nav__button--open")) {
-    navButton.classList.remove("nav__button--open");
-    navList.classList.remove("nav__list--open");
+  if (navButton.classList.contains("nav__button--opened")) {
+    navButton.classList.remove("nav__button--opened");
+    navList.classList.remove("nav__list--opened");
   } else {
-    navButton.classList.add("nav__button--open");
-    navList.classList.add("nav__list--open");
+    navButton.classList.add("nav__button--opened");
+    navList.classList.add("nav__list--opened");
   }
 });
 
 for (let i=0; i < navListItems.length; i++) {
   navListItems[i].addEventListener("click", () => {
-    navButton.classList.remove("nav__button--open");
-    navList.classList.remove("nav__list--open");
+    navButton.classList.remove("nav__button--opened");
+    navList.classList.remove("nav__list--opened");
   });
 }
